4fa73478959ee5e18b6549e4c2b61f6439175b3e
Elutayo <elutayojohnson@gmail.com>
2025-07-17 13:35:20 +0100

    Adding new views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Author, Book, Library, Librarian
 
-# Create your views here.
+
+
+
+def books_by_author(requests, author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+    return render(requests, 'books_by_author.html', {'author': author, 'books': books})
+
+
+
+
