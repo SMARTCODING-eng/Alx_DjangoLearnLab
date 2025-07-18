90274795ec5c9eb2b09e5d25b8467d4ef53cf793
Elutayo <elutayojohnson@gmail.com>
2025-07-18 12:28:15 +0100

    modify

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.decorators import permission_required
+from django import forms
 from .models import Library
 from .models import Book, Author
 from .forms import UserRegistrationForm
