d7086f2bf312baae0f5c850b40f5f1fc381c6174
Elutayo <elutayojohnson@gmail.com>
2025-07-18 11:25:47 +0100

    adding files

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -14,7 +14,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
-    class meta:
+    class Meta:
         permissions = [
             ("can_add_book", "Can add book entry"),
             ("can_edit_book", "Can edit book entry"),
