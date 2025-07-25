465f2f3b1ed722baeb73a589debe1b224a305496
Elutayo <elutayojohnson@gmail.com>
2025-07-25 13:18:59 +0100

    modified

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser, BaseUserManager, Group, Permission
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
@@ -51,6 +51,14 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
 
+    class Meta:
+        permissions = [
+            ("can_view", "can view details of books"),
+            ("can_create", "can create books"),
+            ("can_edit", "can edit books"),
+            ("can_delete", "can delete books"),
+        ]
+
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
@@ -68,16 +76,66 @@ class UserProfile(models.Model):
         related_name='profile',
         verbose_name='User'
         )
+    
+
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='Member')
     bio = models.TextField(blank=True, null=True)
     def __str__(self):
         return f"{self.user.username} ({self.role})"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.update_user_groups()
+
+    def update_user_groups(self):
+        """update the user's groups based on their role"""
+        self.user.groups.clear()
+        if self.role == 'Admin':
+            group, _ = Group.objects.get_or_create(name='Admin')
+            self.user.groups.add(group)
+            self.user.is_staff = True
+            self.user.save()
+
+        elif self.role == 'Librarian':
+            group, _ = Group.objects.get_or_create(name='Editors')
+            self.user.groups.add(group)
+            self.assign_permissions_to_group('Editors', [
+                'can_view',
+                'can_create',
+                'can_edit',
+            ])
+            self.user.save()
+        elif self.role == 'Member':
+            group, _ = Group.objects.get_or_create(name='Viewers')
+            self.user.groups.add(group)
+            self.assign_permissions_to_group('Viewers', [
+                'can_view',
+            ])
+    @staticmethod
+    def assign_permissions_to_group(group_name, permission_codenames):
+        """"assigning permissions to the groups"""
+        group = Group.objects.get(name=group_name)
+        content_type = ContentType.objects.get_for_model(Book)
+        
+        for codename in permission_codenames:
+            permission = permission.objects.get(
+                content_type=content_type,
+                codename=codename
+            )
+            group.permissions.add(permission)
+
 
 @receiver(post_save, sender=CustomUser)
-def save_user_profile(sender, instance, created, **kwargs):
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-    instance.userprofile.save()
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
+
+
 
 
     
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,102 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required, login_required
+from django.contrib import messages
+from .models import Book
 
-# Create your views here.
+
+@permission_required('app_name.can_view', raise_exception=True)
+@login_required
+def book_detail_view(request, book_id):
+    """
+    Views to display book detals.
+    Requires 'can_view' permission.
+    """
+    book = get_object_or_404(Book, pk=book_id)
+    context = {
+        'book':book,
+        'can_edit': request.user.has_perm('bookshelf.can_edit'),
+        'can_delete': request.user.has_perm('bookshelf.can_delete'),
+    }
+    return render(request, 'books/book_details.html', context)
+
+   
+
+
+@permission_required('app_name.can_create', raise_exception=True)
+def book_create_view(request):
+    
+    """
+    View to create a new book.
+    Requires 'can_create' permission.
+    """
+    if request.method == 'POST':
+        try:
+            title = request.POST.get('title')
+            author = request.POST.get('author')
+            publication_year = request.POST.get('publication_year')
+
+            if not title or not author or not publication_year:
+                raise ValueError("All fields are required")
+
+            book = Book.objects.create(
+                title=title,
+                author=author,
+                publication_year=publication_year
+            )
+            messages.success(request, f'Book "{book.title}" created successfully!')
+            return redirect('book_detail', book_id=book.id)
+
+        except Exception as e:
+           messages.error(request, str(e))
+        except Exception as e:
+            messages.error(request, f'Error creating book: {str(e)}')
+    return render(request, 'books/book_create.html')
+
+
+@permission_required('app_name.can_edit', raise_exception=True)
+def book_update_view(request, book_id):
+    """
+    Views to edit an existing book.
+    Requires 'can_edit_book' permission
+    """
+    book = get_object_or_404(Book, pk=book_id)
+    if request.method == 'POST':
+        try:
+            title = request.POST.get('title')
+            author = request.POST.get('author')
+            publication_year = request.POST.get('publication_year')
+
+            if not title or not author or not publication_year:
+                raise ValueError("All fields are required")
+            
+            book.title = title
+            book.author = author
+            book.publication_year = publication_year
+            book.save()
+            messages.success(request, f'Book "{book.title}" updated successfully!')
+            return redirect('book_detail', book_id=book.id)
+        
+        except ValueError as e:
+            messages.error(request, str(e))
+        except Exception as e:
+            messages.error(request, f'Error updating book: {str(e)}')
+
+    return render(request, 'books/book_edit.html', {'book':book})
+
+
+@permission_required('app_name.can_delete', raise_exception=True)
+@login_required
+def book_delete_view(request, book_id):
+    """
+    View to delete a book.
+    Requires 'can_delete_book' permission.
+    """
+    book = get_object_or_404(Book, pk=book_id)
+    
+    if request.method == 'POST':
+        title = book.title
+        book.delete()
+        messages.success(request, f'Book "{title}" deleted successfully!')
+        return redirect('book_list')
+    
+    return render(request, 'books/book_confirm_delete.html', {'book': book})
