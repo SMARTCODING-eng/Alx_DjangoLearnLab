9405f01ec03183202791d575217d2b861001bcfc
SMARTCODING-eng <elutayojohnson@gmail.com>
2025-08-11 00:08:45 +0100

    add

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -4,6 +4,7 @@ from .models import Book
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .serializers import BookSerializer
 from datetime import datetime
+from django_filters import rest_framework
  
 
 class ListView(generics.ListAPIView):
