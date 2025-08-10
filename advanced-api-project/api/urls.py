f0569a0001fc61e3c97ffd4818a0f3f269ee5874
SMARTCODING-eng <elutayojohnson@gmail.com>
2025-08-10 22:30:15 +0100

    modifield

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
     path('books-create/', CreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', UpdateView.as_view(), name='book-detal'),
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='book-update'),
+    path('books/update/', UpdateView.as_view(), name='book-update'),
     path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
 
 ]
