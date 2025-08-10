55c957cca0dace034014193ff853f6a9938f51b1
SMARTCODING-eng <elutayojohnson@gmail.com>
2025-08-10 23:41:52 +0100

    adding

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 
 urlpatterns = [
     path('books/', ListView.as_view(), name='book-list'),
-    path('books-create/', CreateView.as_view(), name='book-create'),
+    path('books/create/', CreateView.as_view(), name='book-create'),
     path('books/<int:pk>/', UpdateView.as_view(), name='book-detal'),
     path('books/update/', UpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='book-delete'),
+    path('books/delete/', DeleteView.as_view(), name='book-delete'),
 
 ]
