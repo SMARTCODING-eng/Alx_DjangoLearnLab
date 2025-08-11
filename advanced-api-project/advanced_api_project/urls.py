1af2b4982c3801267d6bd41ca89571e05c3418f8
Elutayo <elutayojohnson@gmail.com>
2025-08-11 15:54:30 +0100

    modifield

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import urls
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(urls)),
+    path('api/', include('api.urls')),
 ]
