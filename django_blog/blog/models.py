7dcfd2e6e2f8ae95bf042ac58d01cfdcf16331bb
Elutayo <elutayojohnson@gmail.com>
2025-08-13 00:12:15 +0100

    modify

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 
@@ -6,5 +7,5 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
