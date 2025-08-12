73252c089f9603f0475f2ca06885fd86bd95f517
Elutayo <elutayojohnson@gmail.com>
2025-08-13 00:09:05 +0100

    adding a blog project

---
+++ django_blog/blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
