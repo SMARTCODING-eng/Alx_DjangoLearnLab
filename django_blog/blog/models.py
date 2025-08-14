f45f8a4126ee1aeffb59267993a823b0cd073f92
Elutayo <elutayojohnson@gmail.com>
2025-08-14 18:40:37 +0100

    modify

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,12 +12,18 @@ class CustomUser(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='posts')
+    tags = models.ManyToManyField(Tag, related_name='posts')
 
     def __str__(self):
         return f'Post: {self.title} by {self.author.email}'
