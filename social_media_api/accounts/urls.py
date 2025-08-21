80f452df3d97d4ef186c2b38b0f3fc71e83637c8
Elutayo <elutayojohnson@gmail.com>
2025-08-21 14:50:08 +0100

    modify

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
-    path('follow/<str:username>/', FollowUserView.as_view(), name='follow'),
-    path('unfollow/<str:username>/', FollowUserView.as_view(), name='unfollow'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow'),
+    path('unfollow/<int:user_id>/', FollowUserView.as_view(), name='unfollow'),
 ]
 
