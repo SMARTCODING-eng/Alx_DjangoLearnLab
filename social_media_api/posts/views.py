31f840424828d48fdd7641253104d18827f64723
Elutayo <elutayojohnson@gmail.com>
2025-08-21 13:29:06 +0100

    modify

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Post, Comment
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+# from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import permissions
 from .serializers import PostSerializer, CommentSerializer
 from.models import Post, Comment
 from rest_framework import generics
@@ -10,7 +11,7 @@ from rest_framework import generics
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
 
     filterset_fields = ['title', 'content']
     searchset_fields = ['author', 'title', 'content']
@@ -22,7 +23,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticated, IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -32,10 +33,10 @@ class UserFeedView(generics.ListAPIView):
     A view that returns a feed of posts from users the current user follows.
     """
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         followed_users = user.following.all()  
-        queryset = Post.objects.filter(owner__in=followed_users).order_by('-created_at')
+        queryset = Post.objects.filter(author__in=followed_users).order_by('-created_at')
         return queryset
