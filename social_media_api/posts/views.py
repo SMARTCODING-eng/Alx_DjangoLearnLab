8aef57747747babe770a69b1f8227cb96525ccf6
Elutayo <elutayojohnson@gmail.com>
2025-08-22 01:56:36 +0100

    modify

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+# from django.shortcuts import generic
 from .models import Post, Comment
 from rest_framework import viewsets, permissions, status
 from rest_framework.response import Response
@@ -6,12 +6,13 @@ from django.db.utils import IntegrityError
 from .serializers import PostSerializer, CommentSerializer, LikeSerializer
 from.models import Post, Comment, Like
 from rest_framework import generics
+from notifications.models import Notification
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     filterset_fields = ['title', 'content']
     searchset_fields = ['author', 'title', 'content']
@@ -47,11 +48,23 @@ class LikeViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
-    def perform_create(self, serializer):
-        try:
-            serializer.save(author=self.request.user)
-        except IntegrityError:
-            return Response(
-                {"error": "You have already liked this post."},
-                status=status.HTTP_409_CONFLICT
-            )
+    def perform_create(self, request, *args, **kwargs):
+        pk = request.data.get('post')
+        if not pk:
+            return Response({"error": "Post ID required."}, status=status.HTTP_400_BAD_REQUEST)
+        post = generics.get_object_or_404(Post, pk=pk)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
+
+        if not created:
+            return Response({"error": "You have already liked this post."}, status=status.HTTP_409_CONFLICT)
+        
+
+        Notification.objects.create(
+            sender=request.user,
+            recipient=post.author,
+            notification_type='LIKE',
+            post=post
+        )
+
+        serializer = self.get_serializer(like)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
