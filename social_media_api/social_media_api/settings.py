d02927e7d5e06906b892e4d0c21a427a851fbd2e
Elutayo <elutayojohnson@gmail.com>
2025-08-23 20:21:27 +0100

    modify setting

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,6 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-n(jw8q#ad58ka%smc&o0!v+io8ixw-qtw@eh&sk-+hey$%xjvj'
 
+SECURE_BROWSER_XSS_FILTER = True
+X_FRAME_OPTIONS = 'DENY'
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
